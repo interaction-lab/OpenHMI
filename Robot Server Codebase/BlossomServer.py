6f8d71fd1db8113e7b75f6b28d80dfab6773a5df
Kana <orzfirst@vip.qq.com>
2023-01-25 17:24:20 -0800

    Format codes

--- Robot Server Codebase/BlossomServer.py
+++ Robot Server Codebase/BlossomServer.py
@@ -4,40 +4,40 @@ bodyTemplate = r"<html><head><title>Notification Received</title></head><body><p
 
 class BlossomHandler(BaseHTTPRequestHandler):
 
-    def __init__(self, callback, *args):
-        self._callback = callback
-        BaseHTTPRequestHandler.__init__(self, *args)
+	def __init__(self, callback, *args):
+		self._callback = callback
+		BaseHTTPRequestHandler.__init__(self, *args)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        body = bodyTemplate % self.path
-        bodyBinary = bytes(body, "utf-8")
-        self.wfile.write(bodyBinary)
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		body = bodyTemplate % self.path
+		bodyBinary = bytes(body, "utf-8")
+		self.wfile.write(bodyBinary)
 
-        if self._callback and callable(self._callback):
-            self._callback(self.path)
+		if self._callback and callable(self._callback):
+			self._callback(self.path)
 
 class BlossomServer:
 
-    def __init__(self, callback, hostname = "localhost", port=80):
-        self._hostname = hostname
-        self._port = port
-        def handler(*args):
-            BlossomHandler(callback, *args)
-        self._server = HTTPServer((self._hostname, self._port), handler)
+	def __init__(self, callback, hostname = "localhost", port=80):
+		self._hostname = hostname
+		self._port = port
+		def handler(*args):
+			BlossomHandler(callback, *args)
+		self._server = HTTPServer((self._hostname, self._port), handler)
 
-    def run(self):
-        print("Server started at http://%s:%s" % (self._hostname, self._port))
-        with self._server:
-            try:
-                self._server.serve_forever()
-            except KeyboardInterrupt:
-                pass
-        print("Server stopped.")
+	def run(self):
+		print("Server started at http://%s:%s" % (self._hostname, self._port))
+		with self._server:
+			try:
+				self._server.serve_forever()
+			except KeyboardInterrupt:
+				pass
+		print("Server stopped.")
 
-if __name__ == "__main__":     
-    callback = lambda p : print(p)
-    web_server = BlossomServer(callback)
-    web_server.run()
+if __name__ == "__main__":	 
+	callback = lambda p : print(p)
+	web_server = BlossomServer(callback)
+	web_server.run()
